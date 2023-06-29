5d282275ea030957dcb224fbf68f56497d4ff644
AlbertKloster <albert.kloster@gmail.com>
2023-06-29 12:32:42 -0400

    Stage1

--- Main.kt
+++ Main.kt
@@ -1,5 +1,34 @@
 package watermark
 
+import java.awt.Transparency
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+
 fun main() {
-    println("Hello World!")
+    println("Input the image filename:")
+    val file = readln()
+    try {
+        val image: BufferedImage = ImageIO.read(File(file))
+        println("Image file: $file")
+        println("Width: ${image.width}")
+        println("Height: ${image.height}")
+        println("Number of components: ${image.colorModel.numComponents}")
+        println("Number of color components: ${image.colorModel.numColorComponents}")
+        println("Bits per pixel: ${image.colorModel.pixelSize}")
+        println("Transparency: ${image.transparency()}")
+    } catch (e: IOException) {
+        println("The file $file doesn't exist.")
+    }
+
+}
+
+private fun BufferedImage.transparency(): String {
+    return when (transparency) {
+        Transparency.BITMASK -> "BITMASK"
+        Transparency.OPAQUE -> "OPAQUE"
+        Transparency.TRANSLUCENT -> "TRANSLUCENT"
+        else -> "UNKNOWN"
+    }
 }
