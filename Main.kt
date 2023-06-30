b038bba1109887d6863211823b730f91997e4fa4
AlbertKloster <albert.kloster@gmail.com>
2023-06-30 10:30:52 -0400

    Stage5

--- Main.kt
+++ Main.kt
@@ -11,7 +11,7 @@ fun main() {
     try {
         val image: BufferedImage = getImage()
         val watermark: BufferedImage = getWatermark()
-        if (image.width != watermark.width || image.height != watermark.height) throw RuntimeException("The image and watermark dimensions are different.")
+        if (image.width < watermark.width || image.height < watermark.height) throw RuntimeException("The watermark's dimensions are larger.")
         saveWatermarkedImage(getWatermarkedImage(image, watermark))
     } catch (e: RuntimeException) {
         println(e.message)
@@ -22,7 +22,27 @@ private fun getWatermarkedImage(image: BufferedImage, watermark: BufferedImage):
     val alpha = getAlpha(watermark)
     val transparencyColor = getTransparencyColor(watermark)
     val weight = getWeight()
-    return createWatermarkedImage(image, watermark, weight, alpha, transparencyColor)
+    val watermarkPosition = getWatermarkPosition(image, watermark)
+    return createWatermarkedImage(image, watermark, weight, alpha, transparencyColor, watermarkPosition)
+}
+
+private fun getWatermarkPosition(image: BufferedImage, watermark: BufferedImage): Position? {
+    println("Choose the position method (single, grid):")
+    return when (PositionMethods.getPositionMethod(readln())) {
+        PositionMethods.SINGLE -> getSinglePosition(image, watermark)
+        PositionMethods.GRID -> null
+    }
+}
+
+private fun getSinglePosition(image: BufferedImage, watermark: BufferedImage): Position {
+    val diffX = image.width - watermark.width
+    val diffY = image.height - watermark.height
+    println("Input the watermark position ([x 0-$diffX] [y 0-$diffY]):")
+    val input = readln()
+    if (!input.matches(Regex("-?\\d+ -?\\d+"))) throw RuntimeException("The position input is invalid.")
+    val (x, y) = input.split(" ").map { it.toInt() }
+    if (x !in 0..diffX || y !in 0..diffY) throw RuntimeException("The position input is out of range.")
+    return Position(x, y)
 }
 
 private fun saveWatermarkedImage(watermarkedImage: BufferedImage) {
@@ -76,10 +96,7 @@ private fun getAlpha(watermark: BufferedImage) = if (watermark.transparency == 3
 private fun getColor(): Color {
     val input = readln()
     if (!input.matches(Regex("\\d+ \\d+ \\d+"))) throw RuntimeException("The transparency color input is invalid.")
-    val (rString, gString, bString) = input.split(" ")
-    val r = rString.toInt()
-    val g = gString.toInt()
-    val b = bString.toInt()
+    val (r, g, b) = input.split(" ").map { it.toInt() }
     if (r !in 0..255 || g !in 0..255 || b !in 0..255) throw RuntimeException("The transparency color input is invalid.")
     return Color(r, g, b)
 }
@@ -89,22 +106,58 @@ private fun createWatermarkedImage(
     watermark: BufferedImage,
     weight: Int,
     alpha: Boolean = false,
-    transparencyColor: Color?
+    transparencyColor: Color?,
+    watermarkPosition: Position?
 ): BufferedImage {
     val watermarkedImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
     for (x in 0 until watermarkedImage.width) {
         for (y in 0 until watermarkedImage.height) {
-            val watermarkColor = Color(watermark.getRGB(x, y), alpha)
             val imageColor = Color(image.getRGB(x, y))
-            val color = if (watermarkColor.alpha == 0 || watermarkColor == transparencyColor) imageColor else Color(
-                (weight * watermarkColor.red + (100 - weight) * Color(image.getRGB(x, y)).red) / 100,
-                (weight * watermarkColor.green + (100 - weight) * Color(image.getRGB(x, y)).green) / 100,
-                (weight * watermarkColor.blue + (100 - weight) * Color(image.getRGB(x, y)).blue) / 100
-            )
-            watermarkedImage.setRGB(x, y, color.rgb)
+            val watermarkColor = getWatermarkColor(watermark, watermarkPosition, x, y, alpha)
+            val currentColor = getCurrentColor(watermarkColor, transparencyColor, imageColor, weight, image, x, y)
+            watermarkedImage.setRGB(x, y, currentColor.rgb)
         }
     }
     return watermarkedImage
+}
+
+private fun getCurrentColor(
+    watermarkColor: Color,
+    transparencyColor: Color?,
+    imageColor: Color,
+    weight: Int,
+    image: BufferedImage,
+    x: Int,
+    y: Int
+) = if (watermarkColor.alpha == 0 || watermarkColor == transparencyColor) imageColor else Color(
+    (weight * watermarkColor.red + (100 - weight) * Color(image.getRGB(x, y)).red) / 100,
+    (weight * watermarkColor.green + (100 - weight) * Color(image.getRGB(x, y)).green) / 100,
+    (weight * watermarkColor.blue + (100 - weight) * Color(image.getRGB(x, y)).blue) / 100
+)
+
+private fun getWatermarkColor(watermark: BufferedImage, watermarkPosition: Position?, x: Int, y: Int, alpha: Boolean) =
+    if (watermarkPosition == null) getWatermarkColorGrid(watermark, Position(x, y), alpha)
+    else getWatermarkColorPosition(watermark, Position(x, y), watermarkPosition, alpha)
+
+private fun getWatermarkColorGrid(watermark: BufferedImage, currentPixelPosition: Position, alpha: Boolean) =
+    Color(watermark.getRGB(currentPixelPosition.x % watermark.width, currentPixelPosition.y % watermark.height), alpha)
+
+private fun getWatermarkColorPosition(
+    watermark: BufferedImage,
+    currentPixelPosition: Position,
+    watermarkPosition: Position,
+    alpha: Boolean
+): Color {
+    if (currentPixelPosition.x !in watermarkPosition.x until watermarkPosition.x + watermark.width ||
+        currentPixelPosition.y !in watermarkPosition.y until watermarkPosition.y + watermark.height
+    ) return Color(0, 0, 0, 0)
+
+    return Color(
+        watermark.getRGB(
+            currentPixelPosition.x - watermarkPosition.x,
+            currentPixelPosition.y - watermarkPosition.y
+        ), alpha
+    )
 }
 
 private fun readImage(file: String): BufferedImage {
@@ -119,3 +172,18 @@ private fun isNotThreeColorComponents(bufferedImage: BufferedImage) = bufferedIm
 
 private fun isNot24or32bit(bufferedImage: BufferedImage) =
     bufferedImage.colorModel.pixelSize != 24 && bufferedImage.colorModel.pixelSize != 32
+
+enum class PositionMethods(val string: String) {
+    SINGLE("single"), GRID("grid");
+
+    companion object {
+        fun getPositionMethod(input: String): PositionMethods {
+            for (positionMethod in PositionMethods.values()) {
+                if (positionMethod.string == input.lowercase()) return positionMethod
+            }
+            throw RuntimeException("The position method input is invalid.")
+        }
+    }
+}
+
+data class Position(val x: Int, val y: Int)
