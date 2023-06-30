61b12bb615ed7455590cabe73af6356d758fdfbe
AlbertKloster <albert.kloster@gmail.com>
2023-06-30 08:40:43 -0400

    Stage4

--- Main.kt
+++ Main.kt
@@ -9,51 +9,69 @@ import javax.imageio.ImageIO
 fun main() {
 
     try {
-        println("Input the image filename:")
-        val image: BufferedImage = readImage(readln())
-        if (isNotThreeColorComponents(image)) throw RuntimeException("The number of image color components isn't 3.")
-        if (isNot24or32bit(image)) throw RuntimeException("The image isn't 24 or 32-bit.")
-
-        println("Input the watermark image filename:")
-        val watermark: BufferedImage = readImage(readln())
-        if (isNotThreeColorComponents(watermark)) throw RuntimeException("The number of watermark color components isn't 3.")
-        if (isNot24or32bit(watermark)) throw RuntimeException("The watermark isn't 24 or 32-bit.")
-
+        val image: BufferedImage = getImage()
+        val watermark: BufferedImage = getWatermark()
         if (image.width != watermark.width || image.height != watermark.height) throw RuntimeException("The image and watermark dimensions are different.")
-
-        val alpha: Boolean = if (watermark.transparency == 3) {
-            println("Do you want to use the watermark's Alpha channel?")
-            readln().lowercase() == "yes"
-        } else false
-
-        val transparencyColor: Color? = if (watermark.transparency != 3) {
-            println("Do you want to set a transparency color?")
-            if (readln().lowercase() == "yes") {
-                println("Input a transparency color ([Red] [Green] [Blue]):")
-                getColor()
-            } else null
-        } else null
-
-        println("Input the watermark transparency percentage (Integer 0-100):")
-        val input = readln()
-        if (!input.matches(Regex("\\d+"))) throw RuntimeException("The transparency percentage isn't an integer number.")
-
-        val weight = input.toInt()
-        if (weight !in 0..100) throw RuntimeException("The transparency percentage is out of range.")
-
-        println("Input the output image filename (jpg or png extension):")
-        val (outputFileName, outputFileExtension) = readln().split(".")
-        if (!outputFileExtension.matches(Regex("jpg|png"))) throw RuntimeException("The output file extension isn't \"jpg\" or \"png\".")
-
-        val outputImage: BufferedImage = getWatermarkedImage(image, watermark, weight, alpha, transparencyColor)
-
-        ImageIO.write(outputImage, outputFileExtension, File("$outputFileName.$outputFileExtension"))
-        println("The watermarked image $outputFileName.$outputFileExtension has been created.")
-
+        saveWatermarkedImage(getWatermarkedImage(image, watermark))
     } catch (e: RuntimeException) {
         println(e.message)
     }
 }
+
+private fun getWatermarkedImage(image: BufferedImage, watermark: BufferedImage): BufferedImage {
+    val alpha = getAlpha(watermark)
+    val transparencyColor = getTransparencyColor(watermark)
+    val weight = getWeight()
+    return createWatermarkedImage(image, watermark, weight, alpha, transparencyColor)
+}
+
+private fun saveWatermarkedImage(watermarkedImage: BufferedImage) {
+    println("Input the output image filename (jpg or png extension):")
+    val (outputFileName, outputFileExtension) = readln().split(".")
+    if (!outputFileExtension.matches(Regex("jpg|png"))) throw RuntimeException("The output file extension isn't \"jpg\" or \"png\".")
+
+    ImageIO.write(watermarkedImage, outputFileExtension, File("$outputFileName.$outputFileExtension"))
+    println("The watermarked image $outputFileName.$outputFileExtension has been created.")
+}
+
+private fun getWeight(): Int {
+    println("Input the watermark transparency percentage (Integer 0-100):")
+    val input = readln()
+    if (!input.matches(Regex("\\d+"))) throw RuntimeException("The transparency percentage isn't an integer number.")
+
+    val weight = input.toInt()
+    if (weight !in 0..100) throw RuntimeException("The transparency percentage is out of range.")
+    return weight
+}
+
+private fun getWatermark(): BufferedImage {
+    println("Input the watermark image filename:")
+    val watermark: BufferedImage = readImage(readln())
+    if (isNotThreeColorComponents(watermark)) throw RuntimeException("The number of watermark color components isn't 3.")
+    if (isNot24or32bit(watermark)) throw RuntimeException("The watermark isn't 24 or 32-bit.")
+    return watermark
+}
+
+private fun getImage(): BufferedImage {
+    println("Input the image filename:")
+    val image: BufferedImage = readImage(readln())
+    if (isNotThreeColorComponents(image)) throw RuntimeException("The number of image color components isn't 3.")
+    if (isNot24or32bit(image)) throw RuntimeException("The image isn't 24 or 32-bit.")
+    return image
+}
+
+private fun getTransparencyColor(watermark: BufferedImage) = if (watermark.transparency != 3) {
+    println("Do you want to set a transparency color?")
+    if (readln().lowercase() == "yes") {
+        println("Input a transparency color ([Red] [Green] [Blue]):")
+        getColor()
+    } else null
+} else null
+
+private fun getAlpha(watermark: BufferedImage) = if (watermark.transparency == 3) {
+    println("Do you want to use the watermark's Alpha channel?")
+    readln().lowercase() == "yes"
+} else false
 
 private fun getColor(): Color {
     val input = readln()
@@ -66,7 +84,13 @@ private fun getColor(): Color {
     return Color(r, g, b)
 }
 
-private fun getWatermarkedImage(image: BufferedImage, watermark: BufferedImage, weight: Int, alpha: Boolean = false, transparencyColor: Color?): BufferedImage {
+private fun createWatermarkedImage(
+    image: BufferedImage,
+    watermark: BufferedImage,
+    weight: Int,
+    alpha: Boolean = false,
+    transparencyColor: Color?
+): BufferedImage {
     val watermarkedImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
     for (x in 0 until watermarkedImage.width) {
         for (y in 0 until watermarkedImage.height) {
@@ -93,4 +117,5 @@ private fun readImage(file: String): BufferedImage {
 
 private fun isNotThreeColorComponents(bufferedImage: BufferedImage) = bufferedImage.colorModel.numColorComponents != 3
 
-private fun isNot24or32bit(bufferedImage: BufferedImage) = bufferedImage.colorModel.pixelSize != 24 && bufferedImage.colorModel.pixelSize != 32
+private fun isNot24or32bit(bufferedImage: BufferedImage) =
+    bufferedImage.colorModel.pixelSize != 24 && bufferedImage.colorModel.pixelSize != 32
